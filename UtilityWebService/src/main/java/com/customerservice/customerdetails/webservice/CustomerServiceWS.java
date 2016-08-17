c9578a20986e71fa98967d03d15feb4a69347a27
srinivas0190 <srinivas.kf563@gmail.com>
2016-08-17 10:53:20 -0500

    added log4j

--- UtilityWebService/src/main/java/com/customerservice/customerdetails/webservice/CustomerServiceWS.java
+++ UtilityWebService/src/main/java/com/customerservice/customerdetails/webservice/CustomerServiceWS.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.jboss.logging.Logger;
+
 import com.customerservice.customerdetails.model.Customer;
 import com.customerservice.customerdetails.service.CustomerService;
 
@@ -17,15 +19,21 @@ import com.customerservice.customerdetails.service.CustomerService;
 @Produces("application/json")
 public class CustomerServiceWS {
 
+	final static Logger logger = Logger.getLogger(CustomerServiceWS.class);
 	
 	  @Path("{param}")
 	  @GET
 	  @Produces("application/json")
 	public Customer getCustomerDetails(@PathParam("param") int c_id) {
+		  
+		logger.info("Invoked rest call");
 
 		CustomerService custService = new CustomerService();
 
 		Customer customer = custService.getCustomerDetails(c_id);
+		
+		logger.info("Completed Rest Call");
+		logger.info(customer);
 		
 		return customer;
 	}
